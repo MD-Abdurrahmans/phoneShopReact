a7b0ebfa6e12a00dde2a2cbcda8fdbd13fefcedf
artorito22 <abraaz763@gamil.com>
2023-09-24 00:16:25 -0700

    dynamic

--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -1,9 +1,22 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../../components/footer/Footer";
 import Navbar from "../../components/header/Navbar";
+import { useEffect } from "react";
 
 
 export default function Root() {
+
+const loc= useLocation();
+
+ console.log(loc)
+useEffect(()=>{
+
+    loc.pathname;
+    document.title = `phone ${loc.pathname.replace('/','-')}`
+
+},[loc.pathname])
+
+
   return (
     <div className="max-w-[1300px] mx-auto">
          <Navbar></Navbar>
